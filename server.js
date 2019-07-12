32d40462599f5bf60ba96f531d714df35ad3eaea
Calvin Fronda <cfronda209@gmail.com>
2019-07-11 17:19:37 -0700

    register & login complete

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
